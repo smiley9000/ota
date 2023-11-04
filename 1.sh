9acaa0cb5e4089e1b779de6d39b378893bd5a32e
Smiley <59495159+smiley9000@users.noreply.github.com>
2023-11-04 16:59:53 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,8 +2,8 @@
 
 loop=80000
 count=0
-num=924498
-suffix=98
+num=100000
+suffix=00
 datetime=$(date +'%Y%m%d_%H%M%S')
 output_file="ota.txt"
 
@@ -11,7 +11,7 @@ while [ $count -lt $loop ]
 do
     printf -v padded_num "%06d" $num
     printf -v padded_suffix "%02d" $suffix
-    url="https://g2slave-ap-north-01.tclcom.com/36046b76899adec0492c04ad334c70b178def47a/${padded_suffix}/${padded_num}"
+    url="https://g2slave-ap-north-01.tclcom.com/d935b4b1127fbcb17d4f07672088ae1d4fb3b09a/${padded_suffix}/${padded_num}"
 
     content_length=$(curl -m 50 -sI "$url" | awk 'BEGIN {IGNORECASE = 1} /Content-Length/ {print $2}' | tr -d '\r')
     if [ -z "$content_length" ]; then
